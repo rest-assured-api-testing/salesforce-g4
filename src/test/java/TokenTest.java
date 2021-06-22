8e16a9726c8edd1d9657bbd41ac86d9c4f62d201
Gustavo Huanca <gustavozha@gmail.com>
2021-06-22 15:06:16 -0400

    Add test and schema to token

--- src/test/java/TokenTest.java
+++ src/test/java/TokenTest.java
@@ -1,13 +1,31 @@
 import api.*;
+import entities.Token;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class TokenTest {
+    Token token;
+
+    @Test
+    public void statusToken_Successful_200(){
+        ApiResponse apiResponse = ApiManager.executeToken();
+        Assert.assertEquals(apiResponse.getStatusCode(), 200);
+    }
 
     @Test
     public void getToken_Successful_200(){
         ApiResponse apiResponse = ApiManager.executeToken();
+        token=apiResponse.getBody(Token.class);
         Assert.assertEquals(apiResponse.getStatusCode(), 200);
     }
+
+    @Test
+    public void verifyTokenSchema_Successful_200(){
+        ApiResponse apiResponse = ApiManager.executeToken();
+        token=apiResponse.getBody(Token.class);
+        apiResponse.validateBodySchema("schemas/token.json");
+    }
+
+
 }
 
