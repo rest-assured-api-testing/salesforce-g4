2e24b60fbb3051da49861eea87fbc500fcbcc09a
Gustavo Huanca <gustavozha@gmail.com>
2021-06-25 11:41:25 -0400

    Add log to test and implements in GetAllSteps

--- src/test/java/steps/GetAllSteps.java
+++ src/test/java/steps/GetAllSteps.java
@@ -24,18 +24,21 @@ import io.cucumber.java.en.Given;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.apache.http.HttpStatus;
+import org.apache.log4j.Logger;
 import org.testng.Assert;
 
+
 public class GetAllSteps {
+    private Logger log = Logger.getLogger(getClass());
 
     private ApiResponse apiResponse;
 
     protected ApiRequest apiRequest = new ApiRequest();
     protected ApiRequestBuilder apiRequestBuilder = new ApiRequestBuilder();
-    protected AccountResponse accountEndToEndResponse= new AccountResponse();
 
     @Before(value = "@GetAllObjects")
     public void setup(){
+        log.info("I setup the token nad base URL");
         apiResponse = ApiManager.executeToken();
         apiRequest = apiRequestBuilder
                 .baseUri(ParameterEndPoints.URL_BASE+ParameterEndPoints.SOBJECTS)
@@ -44,18 +47,21 @@ public class GetAllSteps {
     }
 
     @Given("^I build \"([^\"]*)\" request$")
-    public void i_build_something_request(String method) throws Throwable {
+    public void i_build_something_request(String method) {
+        log.info("I build the request");
         apiRequest.setMethod(ApiMethod.valueOf(method));
     }
 
     @When("^I execute (.+) request$")
-    public void i_execute_request(String sobject) throws Throwable {
+    public void i_execute_request(String sobject) {
+        log.info("I execute the request");
         apiRequest.setEndpoint(sobject);
         apiResponse = ApiManager.execute(apiRequest);
     }
 
     @Then("^the response status code should be \"([^\"]*)\"$")
-    public void the_response_status_code_should_be_something(String strArg1) throws Throwable {
+    public void the_response_status_code_should_be_something(String strArg1) {
+        log.info("I execute the request");
         Assert.assertEquals(apiResponse.getStatusCode(), HttpStatus.SC_OK);
     }
 }
