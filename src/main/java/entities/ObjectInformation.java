389710864ba5764fe3b6edc090fc1d355bc52a19
Gustavo Huanca <gustavozha@gmail.com>
2021-06-26 00:28:08 -0400

    Add javadoc to ObjectInformation

--- src/main/java/entities/ObjectInformation.java
+++ src/main/java/entities/ObjectInformation.java
@@ -1,3 +1,14 @@
+/**
+ * Copyright (c) 2021 Fundacion Jala.
+ * <p>
+ * This software is the confidential and proprietary information of Fundacion Jala
+ * ("Confidential Information"). You shall not disclose such Confidential
+ * Information and shall use it only in accordance with the terms of the
+ * license agreement you entered into with Fundacion Jala
+ *
+ * @author Gustavo Zacarias Huanca Alconz
+ */
+
 package entities;
 
 public class ObjectInformation {
@@ -5,26 +16,56 @@ public class ObjectInformation {
     private String id;
     private String idDelete;
 
+    /**
+     * Gets token.
+     *
+     * @return a token
+     */
     public String getToken() {
         return token;
     }
 
+    /**
+     * Sets token.
+     *
+     * @param token of user
+     */
     public void setToken(final String token) {
         this.token = token;
     }
 
+    /**
+     * Gets Id of feature or entity.
+     *
+     * @return a String with Id
+     */
     public String getId() {
         return id;
     }
 
+    /**
+     * Sets of Id of feature or entity.
+     *
+     * @param id of feature or entity.
+     */
     public void setId(final String id) {
         this.id = id;
     }
 
+    /**
+     * Gets Id of feature or entity to delete that.
+     *
+     * @return a String with Id
+     */
     public String getIdDelete() {
         return idDelete;
     }
 
+    /**
+     * Sets of Id of feature or entity to delete that.
+     *
+     * @param idDelete of feature or entity
+     */
     public void setIdDelete(final String idDelete) {
         this.idDelete = idDelete;
     }
